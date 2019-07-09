651fc92d03a74378e8d2728765b3d10b9c645e46
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-07-09 18:53:33 +0200

    Connect a state container to the page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import Link from 'next/link';
 
 export default class About extends Component {
+  static async getInitialProps() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return {
+    }
+  }
+
   render() {
     return <div>
       About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,77 @@
 import React, {Component} from 'react';
 import Link from 'next/link';
+import connectToState, {StateContainer} from 'react-connect-state';
+
+interface CounterState {
+  counter: number;
+  loading: boolean;
+}
+
+class Counter extends StateContainer<CounterState> {
+  constructor(initialState: number = 0) {
+    super();
+    this.state = { counter: initialState, loading: initialState === 0 };
+    !initialState && this.getData();
+  }
+
+  private async getData() {
+    await new Promise(r => setTimeout(r, 1000));
+    this.setState({ counter: 23, loading: false });
+  }
+
+  increment = () => {
+    this.setState({counter: this.state.counter + 1 });
+  }
+}
 
 interface IndexProps {
-  foo: string;
+  initialCounter: CounterState;
+}
+
+class CounterView extends Component<{ counter: Counter }> {
+  render() {
+    if (this.props.counter.state.loading) {
+      return <p>Loading...</p>
+    }
+    return <div>
+      Hello {this.props.counter.state.counter}!!!
+      <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+
+
+  private increment = () => {
+    this.props.counter.increment();
+  }
+}
+
+async function wait(cb: () => boolean) {
+  while (!cb()) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
 }
 
 export default class Index extends Component<IndexProps> {
   static async getInitialProps(): Promise<IndexProps> {
-    await new Promise(resolve => setTimeout(resolve, 100));
+    const initialCounter = new Counter();
+
+    if (typeof window === 'undefined') {
+      await wait(() => !initialCounter.state.loading);
+    }
 
     return {
-      foo: 'bar'
+      initialCounter: initialCounter.state
     }
   }
 
+  private readonly counter = new Counter(this.props.initialCounter.counter);
+  private readonly ConnectedCounter = connectToState(CounterView, {counter: this.counter});
+
   render() {
+    const { ConnectedCounter } = this;
+
     return <div>
-      Hello {this.props.foo}!!!
+      <ConnectedCounter />>
       <Link href="/about">
           <a>About</a>
       </Link>
